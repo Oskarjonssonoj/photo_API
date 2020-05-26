c3d98f34a8c64b07908de8e0ea5b3325bc27165b
Oskar Jönsson <peroskarjonsson@gmail.com>
2020-05-26 12:53:51 +0200

    added connection structure to models map

---
+++ controllers/middlewares/auth.js
@@ -0,0 +1,50 @@
+// Authentication middleware
+const { User } = require('../../models')
+
+const basic = async (req, res, next) => {
+	// check if Authorization header exists, otherwise bail
+	if (!req.headers.authorization) {
+		res.status(401).send({
+			status: 'fail',
+			data: 'Authorization required',
+		});
+		return;
+	}
+
+	// "Basic a2FsbGUyMDAwOnNjcmlwdC1raWRxd2Vxd2Vxd2Vxd2Vxd2Vxd2Vxd2U="
+	// =>
+	// [0] = "Basic"
+	// [1] = "a2FsbGUyMDAwOnNjcmlwdC1raWRxd2Vxd2Vxd2Vxd2Vxd2Vxd2Vxd2U="
+	const [authSchema, base64Payload] = req.headers.authorization.split(' ');
+
+	if (authSchema.toLowerCase() !== "basic") {
+		// not ours to authenticate
+		next();
+	}
+
+	const decodedPayload = Buffer.from(base64Payload, 'base64').toString('ascii');
+
+	// kalle:omg-food
+	const [username, password] = decodedPayload.split(':');
+
+	const user = await User.login(username, password);
+	if (!user) {
+		res.status(401).send({
+			status: 'fail',
+			data: 'Authorization failed',
+		});
+		return;
+	}
+
+	// now that we have authenticated the user and know that he/she/it is who it claims to be
+	// attach the user object to the request, so that other parts of the api can use the user
+	req.user = user;
+	req.user.data = { id: user.get('id') }
+
+	next();
+};
+
+
+module.exports = {
+    basic,
+}
---
+++ models/index.js
@@ -0,0 +1,25 @@
+// Setting up the database connection
+const knex = require('knex')({
+      client: 'mysql',
+            connection: {
+                host: process.env.DB_HOST || 'localhost',
+                port: process.env.DB_PORT || 3306,
+                user: process.env.DB_USER || 'root',
+                password: process.env.DB_PASSWORD || '',
+                database: process.env.DB_NAME || 'photo',
+        }
+    });
+
+const bookshelf = require('bookshelf')(knex);
+// const Author = require('./Author')(bookshelf);
+// const Book = require('./Book')(bookshelf);
+// const User = require('./User')(bookshelf);
+
+
+
+module.exports = {
+    bookshelf,
+    // Author,
+    // Book,
+    // User,
+};
